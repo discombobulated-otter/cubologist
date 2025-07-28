ab331fb0b20d20baac025e9601dc76e12d9aa658
smolvamp <kartik14704@gmail.com>
2025-07-29 02:00:57 +0530

    edited homepage url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cubologist/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
